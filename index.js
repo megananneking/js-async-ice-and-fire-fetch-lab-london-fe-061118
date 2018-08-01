9e5300c38df53cc059b3a6b05b1ca56a7fadb6b5
megananneking <megan.king@wework.com>
2018-08-01 19:08:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-  //write fetch request to the Game of Thrones API
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
@@ -12,5 +14,6 @@ function renderBooks(json) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+  var books = fetchBooks();
+  console.log(books);
 })
